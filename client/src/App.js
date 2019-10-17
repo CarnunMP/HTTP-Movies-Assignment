a4c93b64f424890a26418cd3f7f34af23f4cefad
CarnunMP <carnun@hotmail.co.uk>
2019-10-17 17:52:36 +0100

    part one working

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,75 @@
-import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, withRouter } from "react-router-dom";
+import axios from "axios";
+
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovieForm from "./Movies/UpdateMovieForm";
 
-const App = () => {
+const moviesURL = "http://localhost:5000/api/movies";
+
+const App = props => {
+  const [movies, setMovies] = useState([]);
   const [savedList, setSavedList] = useState([]);
+
+  useEffect(() => {
+      axios.get(moviesURL)
+        .then(res => {
+          setMovies(res.data);
+        })
+        .catch(err => {
+          console.log(err.response)
+        });
+  }, []);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
+  const onUpdateSubmit = id => formValues => {
+    const reqBody = {
+      id: id,
+      title: formValues.title,
+      director: formValues.director,
+      metascore: formValues.metascore,
+      stars: JSON.parse(formValues.stars),
+    }
+
+    axios.put(moviesURL + `/${id}`, reqBody)
+      .then(res => {
+        // const rest = movies.filter(movie => movie.id !== id);
+        // setMovies([...rest, res.data]);
+        // ^^^Is there a way to o this without the order changing?
+
+        let newMoviesState = movies;
+        newMoviesState[movies.findIndex(movie => movie.id === id)] = res.data;
+        setMovies(newMoviesState);
+        props.history.push("/");
+      })
+      .catch(err => {
+        alert(err);
+      })
+  }
+
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+      <Route exact path="/" render={props => <MovieList {...props} movies={movies} />} />
       <Route
         path="/movies/:id"
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
       />
+      <Route
+        path="/update-movie/:id"
+        render={props => {
+          return <UpdateMovieForm {...props} movies={movies} onUpdateSubmit={onUpdateSubmit} />
+        }}
+      />
     </>
   );
 };
 
-export default App;
+export default withRouter(App);
